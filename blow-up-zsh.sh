0221b902699acc378e75991cb324f7b092066a4f
nokyoto <nktoooo@proton.me>
2025-04-08 10:40:53 -0400

    blow up blow-up-zsh.sh

--- blow-up-zsh.sh
+++ blow-up-zsh.sh
@@ -1,2 +1,254 @@
+# just copy this code and watch the world burn down
+
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
+r
+r r
 r
 r r
